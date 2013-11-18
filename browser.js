43c4ef26d7585ce8f3c543149051069478e2b4ee
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-18 11:50:59 +0800

    add .Image like in canvas module

--- browser.js
+++ browser.js
@@ -1,7 +1,15 @@
 
-module.exports = function Canvas (w, h) {
+var Canvas = module.exports = function Canvas (w, h) {
   var canvas = document.createElement('canvas')
   canvas.width = w || 300
   canvas.height = h || 150
   return canvas
 }
+
+Canvas.Image = function () {
+  var img = document.createElement('img')
+  return img
+}
+
+
+
